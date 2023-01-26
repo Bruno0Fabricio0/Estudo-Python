a82f154a43ec39917c1364bea69b0c754e41f64a
bruno0fabricio0 <bruno0.fabricio0@gmail.com>
2023-01-25 22:50:06 -0300

    estudando a linguagem

---
+++ minhafuncao.py
@@ -0,0 +1,13 @@
+def faixaIdade(idade):
+    if idade <=12:
+        return 'Crianca'
+    elif idade <=18:
+        return 'Adulto'
+    elif idade <=60:
+        return 'Idoso'
+    else:
+        return 'idoso'
+
+
+def somaQuad(n1,n2):
+    return n1**2 + n2**2
